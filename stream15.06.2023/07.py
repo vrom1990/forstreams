75dcd3e24d4865162230d4e47ca853959af429c7
Вячеслав Романьков <46442686+vrom1990@users.noreply.github.com>
2023-06-15 23:40:41 +0400

    Add files via upload

--- stream15.06.2023/07.py
+++ stream15.06.2023/07.py
@@ -7,10 +7,3 @@
 ##*##*
 '''
 
-for row in range(1, 7):
-    for column in range(1, 7):
-        if column % 3 == 0:
-            print('*', end="")
-        else:
-            print('#', end="")
-    print()
